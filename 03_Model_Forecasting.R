583d7b6f594278b54922cb014d9339e68f6518cd
Jon Goetz <jon.goetz@gov.bc.ca>
2025-04-29 15:09:01 -0700

    test rest of script

--- 03_Model_Forecasting.R
+++ 03_Model_Forecasting.R
@@ -34,9 +34,9 @@ dir.create(output_path)
 # put_bucket(bucket = paste0("rfc-conditions/gw_forecasting/outputs/", as.character(Sys.Date())))
 Regional_group_list <- Regional_group_list[1]
 
-# for(i in Regional_group_list){
+for(i in Regional_group_list){
 
-  i <- Regional_group_list[1]
+  # i <- Regional_group_list[1]
   
     pgown_well_info <- pgown_well_info_all %>%
     # filter(Snow_influenced == 1)%>%
@@ -56,39 +56,34 @@ ensemble_forecast_data <- dl_ensemble_forecast(pgown_well_info,data_location)
 deterministic_forecast_data <- dl_deterministic_forecast(pgown_well_info,data_location)
 
 
-# 
-# 
-# 
-# Time_series_data <- full_join(pgown_data,climate_data) %>%
-#   full_join(snow_data, by = c("Date", "Well")) %>%
-#   filter(Date >= as.Date("2004-01-01")) %>%
-#   pad(by = "Date", group = c("Well")) %>%
-#   group_by(Well) %>%
-#   fill(Snow_influenced, .direction = "downup") %>%
-#   mutate(SWE = ifelse(month(Date) >= 8 & month(Date) <= 10, ifelse(is.na(SWE), 0, SWE), SWE)) %>%
-#   fill(SWE, .direction = "downup") %>%
-#   mutate(groundwater_up_to_last_year = if_else(year(Date) <= year(Sys.Date()) - 1, groundwater, NA_real_)) %>%
-#   mutate(groundwater_period_average = mean(groundwater_up_to_last_year, na.rm = TRUE)) %>%
-#   mutate(groundwater = groundwater_period_average - groundwater) %>%
-#   dplyr::select(-groundwater_up_to_last_year)%>%
-#   ungroup()%>%  
-#     distinct()
-# 
-# 
-# 
-# 
-# #### MODEL ####
-# 
-# source("functions/Forecasting.R")
-# 
-# Model_Forecasting_data <- forecast_model(Time_series_data, forecast_days, num_cores, figure_location,output_path, model_path,pgown_well_info,ensemble_forecast_data,deterministic_forecast_data)
-# 
-# 
-# write_csv(Model_Forecasting_data,paste0(output_path,"/predictive_forecast_results_",i,".csv"))
-# 
-# }
-# 
-# 
-# 
-# 
-# 
+
+
+
+Time_series_data <- full_join(pgown_data,climate_data) %>%
+  full_join(snow_data, by = c("Date", "Well")) %>%
+  filter(Date >= as.Date("2004-01-01")) %>%
+  pad(by = "Date", group = c("Well")) %>%
+  group_by(Well) %>%
+  fill(Snow_influenced, .direction = "downup") %>%
+  mutate(SWE = ifelse(month(Date) >= 8 & month(Date) <= 10, ifelse(is.na(SWE), 0, SWE), SWE)) %>%
+  fill(SWE, .direction = "downup") %>%
+  mutate(groundwater_up_to_last_year = if_else(year(Date) <= year(Sys.Date()) - 1, groundwater, NA_real_)) %>%
+  mutate(groundwater_period_average = mean(groundwater_up_to_last_year, na.rm = TRUE)) %>%
+  mutate(groundwater = groundwater_period_average - groundwater) %>%
+  dplyr::select(-groundwater_up_to_last_year)%>%
+  ungroup()%>%
+    distinct()
+
+
+
+
+#### MODEL ####
+
+source("functions/Forecasting.R")
+
+Model_Forecasting_data <- forecast_model(Time_series_data, forecast_days, num_cores, figure_location,output_path, model_path,pgown_well_info,ensemble_forecast_data,deterministic_forecast_data)
+
+
+write_csv(Model_Forecasting_data,paste0(output_path,"/predictive_forecast_results_",i,".csv"))
+
+}
---
+++ 99_objectstore.R
@@ -0,0 +1,49 @@
+
+
+
+source("01_ConfigInputs.R")
+
+#create a new directory to save the figures too based on the date. 
+
+figure_location <- "Output/"
+
+# Specify the path where you want to create the new folder
+output_path <- paste0(figure_location, "Model_results/", as.character(Sys.Date()))
+
+
+
+library(aws.s3)
+
+
+b <- 'rfc-conditions/gw_forecasting/outputs'
+r <- ""
+
+file_loc <- output_path
+
+output_files <- list.files(file_loc)
+
+for (file_name in output_files) {
+  put_object(paste0(file_loc, file_name),
+             object = file_name,
+             bucket = b, region = r, 
+             acl = "public-read")
+}
+
+
+
+
+# b <- 'rfc-conditions/gw_forecasting/models'
+# r <- ""
+# 
+# file_loc <- "models/"
+# 
+# output_files <- list.files(file_loc)
+# 
+# for (file_name in output_files) {
+#   put_object(paste0(file_loc, file_name),
+#              object = file_name,
+#              bucket=b, region=r,acl = "public-read")
+# }
+# 
+# load(url("https://nrs.objectstore.gov.bc.ca/rfc-conditions/gw_forecasting/models/ANN_Model_OW002_14.Rdata"))
+# 
