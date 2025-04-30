336f9add5cc67ab2e1d6879f9ae98be649be9ff2
Jon Goetz <jon.goetz@gov.bc.ca>
2025-04-30 14:18:50 -0700

    the return of parallel; added knit_root_dir = tmp_dir

--- functions/Forecasting.R
+++ functions/Forecasting.R
@@ -58,23 +58,23 @@ forecast_model <- function(Time_series_data, forecast_days, num_cores, figure_lo
   
   #run calculations for each well in parellel
   
-  # simulated_data <- foreach(y = Well_list, .combine = rbind, 
-  #                           .packages = c("ggpubr", "dplyr",
-  #                                         'ggplot2',
-  #                                         'tidyr',
-  #                                         'lubridate',
-  #                                         'readr',
-  #                                         'stringr',
-  #                                         'tibble',
-  #                                         'forcats',
-  #                                         'purrr',
-  #                                         'gt',
-  #                                         "mgcv", "randomForest","zoo","ggnewscale", "cowplot","nnet")) %dopar% {
-  #                              # filter data by well     
-  #                             #y=Well_list[1]
+  simulated_data <- foreach(y = Well_list, .combine = rbind,
+                            .packages = c("ggpubr", "dplyr",
+                                          'ggplot2',
+                                          'tidyr',
+                                          'lubridate',
+                                          'readr',
+                                          'stringr',
+                                          'tibble',
+                                          'forcats',
+                                          'purrr',
+                                          'gt',
+                                          "mgcv", "randomForest","zoo","ggnewscale", "cowplot","nnet")) %dopar% {
+                               # filter data by well
+                              #y=Well_list[1]
   #                                           
   # for (y in Well_list) {
-  simulated_data <- bind_rows(lapply(Well_list, function(y){
+  # simulated_data <- bind_rows(lapply(Well_list, function(y){
   
    last_measurements_well <- last_measurements %>%
      filter(Well == y) %>%
@@ -2013,6 +2013,7 @@ forecast_model <- function(Time_series_data, forecast_days, num_cores, figure_lo
                                                           legend_plot, rel_widths = c(0.8,0.2)),
                                        "table" = table_gt),
                          output_dir = output_path,
+                         knit_root_dir = tmp_dir,
                          output_file = paste0("Well_", y, "_Model_Predictions.pdf"),
                          intermediates_dir = tmp_dir,  # Isolate temp files
                          envir = new.env())
@@ -2084,8 +2085,8 @@ forecast_model <- function(Time_series_data, forecast_days, num_cores, figure_lo
 
       return(Probabilities_combined_output)
      
-  # } 
-  }))
+  }
+  # }))
   
 }
 
